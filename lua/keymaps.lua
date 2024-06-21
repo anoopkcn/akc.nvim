cc9283745463a026c01125dbe2107f829ed5f8ad
Anoop K. Chandran <anoopkcn@gmail.com>
2024-06-21 13:20:07 +0200

    refactor code to lua directory

---
+++ lua/keymaps.lua
@@ -0,0 +1,2 @@
+vim.keymap.set('n', '<esc>', '<cmd>nohlsearch<cr>')	-- remove search highlight on escape
+
---
+++ lua/options.lua
@@ -0,0 +1,7 @@
+vim.opt.number = true			-- set line number
+vim.opt.undofile = true			-- keep an undo file
+vim.opt.cursorline = true		-- highlight cursor line
+vim.opt.incsearch = true		-- incremental search
+vim.opt.ignorecase = true		-- case insensitive search
+vim.opt.ttyfast = true			-- speed up scrolling in vim
+-- vim.opt.clipboard = 'unnamedplus'	-- combine OS clipboard with vim
