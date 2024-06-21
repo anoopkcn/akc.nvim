09b8c7c87d996400b22870c847b638d6fe0f6c04
Anoop K. Chandran <anoopkcn@gmail.com>
2024-06-21 16:55:38 +0200

    refactor plugins to their own directories

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,16 @@
+return {
+	"nvim-telescope/telescope.nvim",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	config = function()
+		require("telescope").setup({})
+		local builtin = require("telescope.builtin")
+		vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+		vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+		vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+		vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+		-- Shortcut for searching your Neovim configuration files
+		vim.keymap.set("n", "<leader>sn", function()
+			builtin.find_files({ cwd = vim.fn.stdpath("config") })
+		end, { desc = "[S]earch [N]eovim files" })
+	end,
+}
---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,17 @@
+return{
+	"nvim-treesitter/nvim-treesitter",
+	build = "TSUpdate",
+	lazy = false,
+	opts = {
+		ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc', 'python'},
+		auto_install = true,
+		highlight = {
+			enable = true,
+			additional_vim_regex_highlighting = { 'ruby' },
+		},
+		indent = { enable = true, disable = { 'ruby' } },
+	},
+	config = function(_,opts)
+		require('nvim-treesitter.configs').setup(opts)
+	end
+}
