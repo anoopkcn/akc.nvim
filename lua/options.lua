f4f70b6c3a45a0f6f6f5eb865859d34338a7c25d
Anoop K. Chandran <anoopkcn@gmail.com>
2024-06-22 18:06:45 +0200

    remove redundant comments

--- lua/options.lua
+++ lua/options.lua
@@ -1,13 +1,13 @@
-vim.g.mapleader = " " -- set <Space> as leader key
-vim.g.maplocalleader = " " -- set <Space> as local leader key
-vim.g.have_nerd_font = true -- set to true if nerd-font is installed
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+vim.g.have_nerd_font = true
 
-vim.opt.number = true -- set line number
-vim.opt.undofile = true -- keep an undo file
-vim.opt.cursorline = true -- highlight cursor line
-vim.opt.incsearch = true -- incremental search
-vim.opt.ignorecase = true -- case insensitive search
-vim.opt.clipboard = "unnamedplus" -- combine OS clipboard with vim
+vim.opt.number = true
+vim.opt.undofile = true
+vim.opt.cursorline = true
+vim.opt.incsearch = true
+vim.opt.ignorecase = true
+vim.opt.clipboard = "unnamedplus"
 vim.opt.termguicolors = true
 
 vim.opt.tabstop = 4
