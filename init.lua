4b1280f17fce26d3aaa25eaadec848d8f26a20f1
Anoop K. Chandran <anoopkcn@gmail.com>
2024-06-22 17:44:55 +0200

    move lazy init to a separate file

--- init.lua
+++ init.lua
@@ -2,21 +2,7 @@
 -- Author: @anoopkcn
 -- License: MIT
 
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable",
-		lazypath,
-	})
-end
-vim.opt.rtp:prepend(lazypath)
-
 require("options")
 require("keymaps")
-require("lazy").setup("plugins")
-
+require("lazyinit")
 vim.cmd.colorscheme("onedark")
