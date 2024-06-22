a23b00e3c797aeab4034258918e7429bce5a7019
Anoop K. Chandran <anoopkcn@gmail.com>
2024-06-22 21:57:15 +0200

    better comments and disable conflicting keys

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,23 +1,17 @@
 -- https://github.com/neovim/nvim-lspconfig
--- LSP Server Manager
--- https://github.com/williamboman/mason.nvim,
--- https://github.com/williamboman/mason-lspconfig.nvim,
--- Autocomplete Plugin:
--- https://github.com/hrsh7th/nvim-cmp,
--- https://github.com/hrsh7th/cmp-nvim-lsp,
--- https://github.com/hrsh7th/cmp-path,
--- https://github.com/hrsh7th/cmp-buffer,
--- https://github.com/hrsh7th/cmp-cmdline,
+
 return {
 	"neovim/nvim-lspconfig",
 	dependencies = {
-		"williamboman/mason.nvim",
-		"williamboman/mason-lspconfig.nvim",
-		"hrsh7th/nvim-cmp",
-		"hrsh7th/cmp-nvim-lsp",
-		"hrsh7th/cmp-path",
-		"hrsh7th/cmp-buffer",
-		"hrsh7th/cmp-cmdline",
+		-- LSP Server Manager
+		"williamboman/mason.nvim", -- https://github.com/williamboman/mason.nvim
+		"williamboman/mason-lspconfig.nvim", -- https://github.com/williamboman/mason-lspconfig.nvim
+		-- Autocomplete Plugin:
+		"hrsh7th/nvim-cmp", -- https://github.com/hrsh7th/nvim-cmp
+		"hrsh7th/cmp-nvim-lsp", -- https://github.com/hrsh7th/cmp-nvim-lsp
+		"hrsh7th/cmp-path", -- https://github.com/hrsh7th/cmp-path
+		"hrsh7th/cmp-buffer", -- https://github.com/hrsh7th/cmp-buffer
+		"hrsh7th/cmp-cmdline", -- https://github.com/hrsh7th/cmp-cmdline
 	},
 	config = function()
 		local lspconfig = require("lspconfig")
@@ -32,7 +26,7 @@ return {
 			ensure_installed = { "pyright", "lua_ls", "rust_analyzer", "tsserver" },
 		})
 
-		-- combine the default capabilities with the capabilities of the nvim-cmp
+		-- combine the default capabilities with the nvim-cmp capabilities
 		local capabilities = vim.tbl_deep_extend(
 			"force",
 			{},
@@ -49,7 +43,8 @@ return {
 			settings = {
 				Lua = {
 					diagnostics = {
-						globals = { "vim" }, -- set vim as global variable oterwise it will show warnings in lua files
+						-- Get the language server to recognize the `vim` global
+						globals = { "vim" },
 					},
 				},
 			},
@@ -58,12 +53,12 @@ return {
 		-- autocomplete setup
 		local cmp_select = { behavior = cmp.SelectBehavior.Select }
 		cmp.setup({
-			enabled = true, -- enabled by default
+			enabled = true,
 			mapping = cmp.mapping.preset.insert({
 				["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
 				["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
 				["<C-y>"] = cmp.mapping.confirm({ select = true }),
-				["<C-Space>"] = cmp.mapping.complete(),
+				-- ["<C-Space>"] = cmp.mapping.complete(),
 			}),
 			-- load sources for autocomplete that is from buffers and lsp servers
 			sources = cmp.config.sources({ { name = "nvim_lsp" } }, { { name = "buffer" } }, { { name = "path" } }),
