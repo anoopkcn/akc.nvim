843ea43ac9ba933b9d689630f582738723446fcd
Anoop K. Chandran <anoopkcn@gmail.com>
2024-06-23 05:40:09 +0200

    small update

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -23,8 +23,8 @@ return {
 		vim.keymap.set("n", "<leader>gb", builtin.git_branches, {})
 		vim.keymap.set("n", "<leader>gl", builtin.git_bcommits, {})
 		-- Shortcut for searching your Neovim configuration files
-		vim.keymap.set("n", "<leader>sn", function()
+		vim.keymap.set("n", "<leader>ni", function()
 			builtin.find_files({ cwd = vim.fn.stdpath("config") })
-		end, { desc = "[S]earch [N]eovim files" })
+		end, { desc = "[N]eovim [I]nit files" })
 	end,
 }
