5a2f2e47384e13289f6e1c44fb74a0e066f813dd
Anoop K. Chandran <anoopkcn@gmail.com>
2024-06-25 23:38:33 +0200

    keymap settings for lsp config

--- lua/plugins/lsp_mason.lua
+++ lua/plugins/lsp_mason.lua
@@ -40,5 +40,32 @@ return {
 				},
 			},
 		})
+		vim.api.nvim_create_autocmd('LspAttach', {
+			desc = 'LSP actions',
+			callback = function()
+				local bufmap = function(mode, lhs, rhs)
+					local opts = { buffer = true }
+					vim.keymap.set(mode, lhs, rhs, opts)
+				end
+
+				-- Displays hover information about the symbol under the cursor
+				bufmap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>')
+
+				-- Jump to the definition
+				bufmap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>')
+
+				-- Jump to declaration
+				bufmap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>')
+
+				-- Lists all the references
+				bufmap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>')
+
+				-- Renames all references to the symbol under the cursor
+				bufmap('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>')
+			end
+		})
+		vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+			border = "rounded",
+		})
 	end,
 }
