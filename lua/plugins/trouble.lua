cb507d6c313179acabcf3d551ee34a8997a01b86
Anoop K. Chandran <anoopkcn@gmail.com>
2024-06-25 13:41:46 +0200

    add trouble with basic settings

---
+++ lua/plugins/trouble.lua
@@ -0,0 +1,29 @@
+return {
+	"folke/trouble.nvim",
+	opts = {
+		auto_close = true, -- autoclose when there are no items
+	},
+	cmd = "Trouble",
+	keys = {
+		{
+			"<leader>tt",
+			"<cmd>Trouble diagnostics toggle<cr>",
+			desc = "Diagnostics (Trouble)",
+		},
+		{
+			"<leader>tx",
+			"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+			desc = "Buffer Diagnostics (Trouble)",
+		},
+		{
+			"<leader>ts",
+			"<cmd>Trouble symbols toggle focus=false<cr>",
+			desc = "Symbols (Trouble)",
+		},
+		{
+			"<leader>tl",
+			"<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+			desc = "LSP Definitions / references / ... (Trouble)",
+		},
+	},
+}
