73f900177681246883e641db2654e7fc3fce50f9
Anoop K. Chandran <anoopkcn@gmail.com>
2024-06-25 13:41:03 +0200

    add comment to the remapkey

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,2 +1,2 @@
-vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>")
+vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>")    -- since Space is the leader key do nothing
 vim.keymap.set("n", "<Esc>", "<Cmd>nohlsearch<CR>") -- remove search highlight on escape
