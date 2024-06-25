5220ab78c6698f2948fa8bfc003526e0a93c1537
Anoop K. Chandran <anoopkcn@gmail.com>
2024-06-25 20:11:40 +0200

    remove unused function

--- lua/core/globals.lua
+++ lua/core/globals.lua
@@ -3,9 +3,3 @@ Print = function(v)
 	print(vim.inspect(v))
 	return v
 end
-
-Lookup = function(string)
-	-- given a string open browser and look up the string
-	-- using the default search engine
-	-- local search_engine = "https://www.google.com/search?q="
-end
