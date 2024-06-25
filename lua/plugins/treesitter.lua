516e4b8531e72a82a54d8a4008a48f03818c654c
Anoop K. Chandran <anoopkcn@gmail.com>
2024-06-25 23:48:24 +0200

    formatting fix

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,6 +3,7 @@ return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
 	lazy = false,
+
 	opts = {
 		ensure_installed = {
 			"bash",
@@ -18,13 +19,17 @@ return {
 			"javascript",
 			"typescript",
 		},
+
 		auto_install = true,
+
 		highlight = {
 			enable = true,
 			additional_vim_regex_highlighting = { "ruby" },
 		},
+
 		indent = { enable = true, disable = { "ruby" } },
 	},
+
 	config = function(_, opts)
 		require("nvim-treesitter.configs").setup(opts)
 	end,
