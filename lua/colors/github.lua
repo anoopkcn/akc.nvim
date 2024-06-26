97436344a98c59bad0b5e9c9beef3c16f6c4ebea
Anoop K. Chandran <anoopkcn@gmail.com>
2024-06-26 21:09:56 +0200

    github default theme

--- lua/colors/github.lua
+++ lua/colors/github.lua
@@ -4,15 +4,6 @@ return {
 	priority = 1000, -- make sure to load this before all the other start plugins
 	enabled = true,
 	config = function()
-		require("github-theme").setup({
-			-- palettes = {
-			-- 	github_dark_default = {
-			-- 		bg1 = "#000000", -- Black background
-			-- 		bg0 = "#1d1d2b", -- Alt backgrounds (floats, statusline, ...)
-			-- 	},
-			-- },
-			-- specs = {},
-			-- groups = {},
-		})
+		require("github-theme").setup({})
 	end,
 }
