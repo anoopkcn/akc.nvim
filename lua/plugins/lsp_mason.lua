dbda7166e4b936b0908a6634f044b83e6c9a8a1d
Anoop K. Chandran <anoopkcn@gmail.com>
2024-06-26 02:28:29 +0200

    better lsp settup

--- lua/plugins/lsp_mason.lua
+++ lua/plugins/lsp_mason.lua
@@ -5,67 +5,78 @@ return {
 	dependencies = {
 		"williamboman/mason.nvim", -- https://github.com/williamboman/mason.nvim
 		"williamboman/mason-lspconfig.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim", --https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim
 		"hrsh7th/cmp-nvim-lsp", -- https://github.com/hrsh7th/cmp-nvim-lsp
 	},
 
 	config = function()
-		local lspconfig = require("lspconfig")
-		local mason = require("mason")
-		local mason_lspconfig = require("mason-lspconfig")
-		local cmp_lsp = require("cmp_nvim_lsp")
-
-		mason.setup()
-		mason_lspconfig.setup(
-			{
-				ensure_installed = { "lua_ls", "rust_analyzer", "pyright" },
-			}
-		)
-
-		-- combine the default capabilities with the nvim-cmp capabilities
-		local capabilities = vim.tbl_deep_extend(
-			"force",
-			{},
-			vim.lsp.protocol.make_client_capabilities(), -- default capabilities
-			cmp_lsp.default_capabilities()      -- nvim-cmp capabilities
-		)
-
-		-- lspconfig setup for each language server
-		lspconfig["pyright"].setup({ capabilities = capabilities })
-		lspconfig["rust_analyzer"].setup({ capabilities = capabilities })
-		lspconfig["lua_ls"].setup({
-			capabilities = capabilities,
-			settings = {
-				Lua = {
-					diagnostics = {
-						globals = { "vim" },
+		local servers = {
+			pyright = {},
+			rust_analyzer = {},
+			-- tsserver = {},
+			lua_ls = {
+				-- capabilities = {},
+				settings = {
+					Lua = {
+						completion = {
+							callSnippet = "Replace",
+						},
+						diagnostics = {
+							globals = { "vim" },
+							-- disable = { 'missing-fields' }
+						},
 					},
 				},
 			},
+		}
+
+		require("mason").setup()
+
+		local ensure_installed = vim.tbl_keys(servers or {})
+		vim.list_extend(ensure_installed, {
+			"stylua", -- Used to format Lua code
+			"black", -- Used to format Python code
 		})
 
-		vim.api.nvim_create_autocmd('LspAttach', {
-			desc = 'LSP actions',
+		require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
+
+		-- combine the default capabilities with the nvim-cmp capabilities
+		local capabilities = vim.lsp.protocol.make_client_capabilities()
+		capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
+
+		require("mason-lspconfig").setup({
+			handlers = {
+				function(server_name)
+					local server = servers[server_name] or {}
+					server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
+					require("lspconfig")[server_name].setup(server)
+				end,
+			},
+		})
+
+		vim.api.nvim_create_autocmd("LspAttach", {
+			desc = "LSP actions",
 			callback = function()
-				local bufmap = function(mode, lhs, rhs)
+				local _map = function(mode, lhs, rhs)
 					local opts = { buffer = true }
 					vim.keymap.set(mode, lhs, rhs, opts)
 				end
 
 				-- Displays hover information about the symbol under the cursor
-				bufmap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>')
+				_map("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>")
 
 				-- Jump to the definition
-				bufmap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>')
+				_map("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>")
 
 				-- Jump to declaration
-				bufmap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>')
+				_map("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>")
 
 				-- Lists all the references
-				bufmap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>')
+				_map("n", "gr", "<cmd>lua vim.lsp.buf.references()<cr>")
 
 				-- Renames all references to the symbol under the cursor
-				bufmap('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>')
-			end
+				_map("n", "<F2>", "<cmd>lua vim.lsp.buf.rename()<cr>")
+			end,
 		})
 
 		-- lsp symbol definition window(activated by K) should have border and rounded corner
