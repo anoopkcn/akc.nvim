1e0bf70747a49c2f2a8d83bea7362066d843dc93
Anoop K. Chandran <anoopkcn@gmail.com>
2024-06-26 03:37:07 +0200

    remove refactored files

--- lua/plugins/fugitive.lua
+++
@@ -1,8 +0,0 @@
--- https://github.com/tpope/vim-fugitive
-return {
-	"tpope/vim-fugitive",
-	enabled = true,
-	config = function()
-		vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
-	end,
-}
--- lua/plugins/surround.lua
+++
@@ -1,8 +0,0 @@
--- https://github.com/kylechui/nvim-surround
-return {
-	"kylechui/nvim-surround",
-	version = "*",
-	event = "VeryLazy",
-	enabled = true,
-	config = true,
-}
