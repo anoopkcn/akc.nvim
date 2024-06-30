fef42e467eda8227a0f02de1295f621939af65b2
Anoop K. Chandran <anoopkcn@gmail.com>
2024-06-30 03:06:54 +0200

    enable activate for easy disable

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -25,10 +25,12 @@ return {
 	{
 		-- [surround](https://github.com/tpope/vim-surround)(surround motion for pairs)
 		"tpope/vim-surround",
+		enabled = true,
 	},
 	{
 		-- [comment.nvim](https://github.com/numToStr/Comment.nvim) (add line/block comments easily)
 		"numToStr/Comment.nvim",
+		enabled = true,
 		opts = {},
 	},
 }
